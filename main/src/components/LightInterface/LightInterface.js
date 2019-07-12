72b8c0a9e771f92953a9d7ec466d9ccba97c8b3b
erikmjohnson <erikarisma98@gmail.com>
2019-07-11 21:05:14 -0700

    light buttons work!

--- main/src/components/LightInterface/LightInterface.js
+++ main/src/components/LightInterface/LightInterface.js
@@ -1,6 +1,22 @@
+import superagent from 'superagent';
 import React, {Component} from 'react';
 
+const API_URL = 'http://localhost:3001/';
+const GROUP = `lightgroup/`;
+const LIGHT = 'light/';
+
+
 export default class LightInterface extends Component {
+
+  allLights = (command) => {
+    return superagent.get(`${API_URL}${GROUP}${command}`)
+      .catch(err => console.log(err));
+  };
+
+  light = (id, command) => {
+    return superagent.get(`${API_URL}${LIGHT}${id}/${command}`)
+      .catch(err => console.log(err));
+  };
 
   render() {
     return (
@@ -8,34 +24,34 @@ export default class LightInterface extends Component {
         <h1>Light Settings</h1>
         <ul>
           <li>
-            <button type='submit'>Turn On All Lights</button>
+            <button onClick={this.allLights.bind(null, 'on')}>Turn On All Lights</button>
           </li>
           <li>
-            <button type='submit'>Turn Off All Lights</button>
+            <button onClick={this.allLights.bind(null, 'off')}>Turn Off All Lights</button>
           </li>
           <li>
-            <button type='submit'>Turn On Light 1</button>
+            <button onClick={this.light.bind(null, 1, 'on')}>Turn On Light 1</button>
           </li>
           <li>
-            <button type='submit'>Turn On Light 2</button>
+            <button onClick={this.light.bind(null, 2, 'on')}>Turn On Light 2</button>
           </li>
           <li>
-            <button type='submit'>Turn On Light 6</button>
+            <button onClick={this.light.bind(null, 6, 'on')}>Turn On Light 6</button>
           </li>
           <li>
-            <button type='submit'>Turn On Light 7</button>
+            <button onClick={this.light.bind(null, 7, 'on')}>Turn On Light 7</button>
           </li>
           <li>
-            <button type='submit'>Turn Off Light 1</button>
+            <button onClick={this.light.bind(null, 1, 'off')}>Turn Off Light 1</button>
           </li>
           <li>
-            <button type='submit'>Turn Off Light 2</button>
+            <button onClick={this.light.bind(null, 2, 'off')}>Turn Off Light 2</button>
           </li>
           <li>
-            <button type='submit'>Turn Off Light 6</button>
+            <button onClick={this.light.bind(null, 6, 'off')}>Turn Off Light 6</button>
           </li>
           <li>
-            <button type='submit'>Turn Off Light 7</button>
+            <button onClick={this.light.bind(null, 7, 'off')}>Turn Off Light 7</button>
           </li>
         </ul>
       </div>
